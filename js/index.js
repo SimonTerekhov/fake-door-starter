a3f7cce26a0c6e70cb0aea0adf1509bb94013640
Simkhov <simon.terekhov@gmail.com>
2022-10-20 18:54:50 +0200

    add confetti click

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
+import JSConfetti from '../node_modules/js-confetti/dist/es/index.js';
+
+const jsConfetti = new JSConfetti();
 
 const init = () => {
   const $form = document.querySelector("form");
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
+    jsConfetti.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    });
   });
 }
-
 
 init();
